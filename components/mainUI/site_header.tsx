799a306b3780afbdf6d3228a17222712783f6adf
Luis Ruiz <ruiztechservices@gmail.com>
2024-06-18 04:31:25 -0400

    made header carousel responsive

--- components/mainUI/site_header.tsx
+++ components/mainUI/site_header.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
-
-
-export default function Siteheader() {
+export default function SiteHeader() {
   const [images, setImages] = useState([
     { src: '/images/IMG_9997.jpg', width: 400, height: 400 },
     { src: '/images/IMG_8718.jpg', width: 400, height: 400 },
@@ -21,15 +19,21 @@ export default function Siteheader() {
   }, [images.length]);
 
   return (
-
     <div className="bg-black h-[1000px] lg:h-[800px] relative overflow-hidden">
       <img src='/images/logo-1.png' alt="main-logo" className="w-[200px] md:w-[15em] md:absolute md:top-[67px] md:left-[57px] md:w-[300px] md:h-[300px]" />
-      <div className='z-10 absolute top-[100px] left-[400px] w-[250px] md:w-80 h-[400px] md:h-[450px] overflow-hidden'>
+      <div className='hidden md:block z-10 absolute top-[100px] left-[400px] w-[250px] md:w-80 h-[400px] md:h-[450px] overflow-hidden'>
         {images.map((image, index) => (
-          <Image key={index} src={image.src} width={image.width} height={image.height} alt="customer photo" className={`transition-transform duration-1000 ease-in-out transform ${index === currentIndex ? 'translate-y-0' : '-translate-y-full'} absolute top-0 left-0 w-full h-full object-cover rounded-2xl`} />
+          <Image key={index} src={image.src} width={image.width} height={image.height} alt="customer photo" className={` transition-transform duration-1000 ease-in-out transform ${index === currentIndex ? 'translate-y-0' : '-translate-y-full'} absolute top-0 left-0 w-full h-full object-cover rounded-2xl`} />
         ))}
       </div>
-      <div className="z-0 left-[35px] top-[375px] w-[250px] h-[400px] absolute md:h-[430px] md:w-80 md:top-[150px] md:left-[370px] rounded-xl bg-yellow-300" />{/*<=====HOw do I move this behind the image? I am using `z-index` but it s not working...*/}
+      <div className='block md:hidden z-10 absolute top-[330px] left-20 w-[250px] md:w-80 h-[400px] md:h-[450px] overflow-hidden'>
+        {/* Images for mobile: visible on screens smaller than 768px */}
+        {images.map((image, index) => (
+          <Image key={index} src={image.src} width={image.width} height={image.height} alt="customer photo" className={` transition-transform duration-1000 ease-in-out transform ${index === currentIndex ? 'translate-y-0' : '-translate-y-full'} absolute top-0 left-0 w-full h-full object-cover rounded-2xl`} />
+        ))}
+      </div>
+        
+      <div className="z-0 left-[35px] top-[375px] w-[250px] h-[400px] absolute md:h-[430px] md:w-80 md:top-[150px] md:left-[370px] rounded-xl bg-yellow-300" />
       <img src="/images/logo-4.png" alt="osvaldy the barber logo" className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-[30em] absolute top-[650px] md:top-[7px] md:left-[580px] lg:w-[800px] lg:h-[800px]" />
     </div>
   )
