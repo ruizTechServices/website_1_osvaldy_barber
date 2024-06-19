7cf68a8c84f003c577d38efcceb12ccb7eb68288
Luis Ruiz <ruiztechservices@gmail.com>
2024-06-19 00:47:32 -0400

    created navbar

---
+++ components/mainUI/navbar.tsx
@@ -0,0 +1,70 @@
+// C:\Users\Gio\OneDrive\Desktop\ruizTechServices\osvaldy_barber\websites\nextjs\website_1_osvaldy_barber\components\mainUI\navbar.tsx
+"use client";
+import { useState } from 'react';
+
+const BurgerNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="w-fit text-white absolute top-10  left-[300px] md:top-0 md:left-[1200px] z-10">
+      <div className="flex justify-between items-center p-4">
+        <div>
+          <button
+            onClick={toggleMenu}
+            className="focus:outline-none menu-button"
+            aria-expanded={isOpen}
+            aria-label="Toggle menu"
+          >
+            <div className="w-6 h-6 flex flex-col justify-between items-center">
+              <span
+                className={`block h-1 w-full bg-white transition-transform duration-300 ease-in-out ${isOpen ? 'transform rotate-45 translate-y-2' : ''}`}
+              />
+              <span
+                className={`block h-1 w-full bg-white transition-opacity duration-300 ease-in-out ${isOpen ? 'opacity-0' : ''}`}
+              />
+              <span
+                className={`block h-1 w-full bg-white transition-transform duration-300 ease-in-out ${isOpen ? 'transform -rotate-45 -translate-y-2' : ''}`}
+              />
+            </div>
+          </button>
+        </div>
+      </div>
+
+      <div
+        className={`fixed top-0 left-0 w-[300px] h-screen bg-gray-800 text-white flex flex-col justify-center items-center z-50 overflow-hidden transition-transform duration-300 ${isOpen ? 'transform translate-x-0' : 'transform -translate-x-full'}`}
+      >
+        <a href="#" className="mb-4 text-xl">Link 1</a>
+        <a href="#" className="mb-4 text-xl">Link 2</a>
+        <a href="#" className="mb-4 text-xl">Link 3</a>
+        <a href="#" className="mb-4 text-xl">Link 4</a>
+        <div className="absolute bottom-4">
+          <div className="text-2xl font-bold">Logo</div>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .menu-button span {
+          transition: all 0.3s ease;
+        }
+
+        .menu-button span:nth-child(1) {
+          transform: ${isOpen ? 'rotate(45deg) translate(5px, 10px)' : 'rotate(0)'};
+        }
+
+        .menu-button span:nth-child(2) {
+          opacity: ${isOpen ? 0 : 1};
+        }
+
+        .menu-button span:nth-child(3) {
+          transform: ${isOpen ? 'rotate(-45deg) translate(5px, -8px)' : 'rotate(0)'};
+        }
+      `}</style>
+    </nav>
+  );
+};
+
+export default BurgerNavbar;
--- components/mainUI/site_header.tsx
+++ components/mainUI/site_header.tsx
@@ -2,39 +2,72 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import BurgerNavbar from './navbar';
 
 export default function SiteHeader() {
   const [images, setImages] = useState([
-    { src: '/images/IMG_9997.jpg', width: 400, height: 400 },
-    { src: '/images/IMG_8718.jpg', width: 400, height: 400 },
-    { src: '/images/IMG_8598.jpg', width: 400, height: 400 },
+    { src: "/images/IMG_9997.jpg", width: 400, height: 400 },
+    { src: "/images/IMG_8718.jpg", width: 400, height: 400 },
+    { src: "/images/IMG_8598.jpg", width: 400, height: 400 }
   ]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((currentIndex) => (currentIndex + 1) % images.length);
-    }, 3000); // Change image every 3000 milliseconds (3 seconds)
-    return () => clearInterval(interval);
-  }, [images.length]);
+  useEffect(
+    () => {
+      const interval = setInterval(() => {
+        setCurrentIndex(currentIndex => (currentIndex + 1) % images.length);
+      }, 3000); // Change image every 3000 milliseconds (3 seconds)
+      return () => clearInterval(interval);
+    },
+    [images.length]
+  );
 
   return (
     <div className="bg-black h-[1000px] lg:h-[800px] relative overflow-hidden">
-      <img src='/images/logo-1.png' alt="main-logo" className="w-[200px] md:w-[15em] md:absolute md:top-[67px] md:left-[57px] md:w-[300px] md:h-[300px]" />
-      <div className='hidden md:block z-10 absolute top-[100px] left-[400px] w-[250px] md:w-80 h-[400px] md:h-[450px] overflow-hidden'>
-        {images.map((image, index) => (
-          <Image key={index} src={image.src} width={image.width} height={image.height} alt="customer photo" className={` transition-transform duration-1000 ease-in-out transform ${index === currentIndex ? 'translate-y-0' : '-translate-y-full'} absolute top-0 left-0 w-full h-full object-cover rounded-2xl`} />
-        ))}
+      <img
+        src="/images/logo-1.png"
+        alt="main-logo"
+        className="w-[200px] md:w-[15em] md:absolute md:top-[67px] md:left-[57px] md:w-[300px] md:h-[300px]"
+      />
+      <div className="hidden md:block z-10 absolute top-[100px] left-[400px] w-[250px] md:w-80 h-[400px] md:h-[450px] overflow-hidden">
+        {images.map((image, index) =>
+          <Image
+            key={index}
+            src={image.src}
+            width={image.width}
+            height={image.height}
+            alt="customer photo"
+            className={` transition-transform duration-1000 ease-in-out transform ${index ===
+            currentIndex
+              ? "translate-y-0"
+              : "-translate-y-full"} absolute top-0 left-0 w-full h-full object-cover rounded-2xl`}
+          />
+        )}
       </div>
-      <div className='block md:hidden z-10 absolute top-[330px] left-20 w-[250px] md:w-80 h-[400px] md:h-[450px] overflow-hidden'>
+      <div className="block md:hidden z-10 absolute top-[330px] left-20 w-[250px] md:w-80 h-[400px] md:h-[450px] overflow-hidden">
         {/* Images for mobile: visible on screens smaller than 768px */}
-        {images.map((image, index) => (
-          <Image key={index} src={image.src} width={image.width} height={image.height} alt="customer photo" className={` transition-transform duration-1000 ease-in-out transform ${index === currentIndex ? 'translate-y-0' : '-translate-y-full'} absolute top-0 left-0 w-full h-full object-cover rounded-2xl`} />
-        ))}
+        {images.map((image, index) =>
+          <Image
+            key={index}
+            src={image.src}
+            width={image.width}
+            height={image.height}
+            alt="customer photo"
+            className={` transition-transform duration-1000 ease-in-out transform ${index ===
+            currentIndex
+              ? "translate-y-0"
+              : "-translate-y-full"} absolute top-0 left-0 w-full h-full object-cover rounded-2xl`}
+          />
+        )}
       </div>
-        
+
       <div className="z-0 left-[35px] top-[375px] w-[250px] h-[400px] absolute md:h-[430px] md:w-80 md:top-[150px] md:left-[370px] rounded-xl bg-yellow-300" />
-      <img src="/images/logo-4.png" alt="osvaldy the barber logo" className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-[30em] absolute top-[650px] md:top-[7px] md:left-[580px] lg:w-[800px] lg:h-[800px]" />
+      <BurgerNavbar    />
+      <img
+        src="/images/logo-4.png"
+        alt="osvaldy the barber logo"
+        className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none w-[30em] absolute top-[650px] md:top-[7px] md:left-[580px] lg:w-[800px] lg:h-[800px]"
+      />
     </div>
-  )
+  );
 }
