ae903ee3056f854b6b812f4321bb4bb01bdc3045
Luis Ruiz <ruiztechservices@gmail.com>
2024-06-19 02:47:55 -0400

    design updates

--- components/mainUI/carousel_main.tsx
+++ components/mainUI/carousel_main.tsx
@@ -9,13 +9,10 @@ const images = [
   "/images/6.png",
   "/images/7.png",
   "/images/8.png",
-  "/images/9.png",
   "/images/10.png",
   "/images/11.png",
   "/images/12.png",
   "/images/13.png",
-  "/images/haircut-1.png",
-  "/images/haircut-2.png",
   "/images/IMG_2378.png",
   "/images/IMG_5873.jpg",
   "/images/IMG_8598.jpg",
@@ -38,10 +35,10 @@ const Carousel = () => {
 
   return (
     <div className="h-[600px] w-full flex flex-col items-center justify-center relative overflow-hidden bg-[url('/images/backgroundbarber.png')]">
-      <div className="w-[600px] flex justify-center items-center overflow-hidden bg-black">
-        <img className="h-[500px] max-w-[600px]" src={images[currentIndex]} alt="Carousel item" />
+      <div className="md:w-[600px] flex justify-center items-center overflow-hidden">
+        <img className="max-h-[300px] max-w-[300px]" src={images[currentIndex]} alt="Carousel item" />
       </div>
-      <div className='flex justify-between items-center w-[600px]'>
+      <div className='flex justify-between items-center md:w-[600px]'>
         <button onClick={prevSlide} className="left-4 top-1/2 transform -translate-y-1/2 bg-yellow-400 text-white font-bold text-3xl p-2 rounded-full">{'<'}</button>
         <button onClick={nextSlide} className="right-4 top-1/2 transform -translate-y-1/2 bg-yellow-400 text-white font-bold text-3xl p-2 rounded-full">{'>'}</button>
       </div>
