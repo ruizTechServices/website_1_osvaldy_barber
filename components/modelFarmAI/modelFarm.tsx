101b2baf8b6a19e894c68e4b4af79bca5d8af765
Luis Ruiz <114837126+ruizTechServices@users.noreply.github.com>
2024-07-04 01:57:02 +0000

    deleted folder

--- components/modelFarmAI/modelFarm.tsx
+++
@@ -1,9 +0,0 @@
-import * as replitai from '@replit/ai-modelfarm';
-
-const result = await replitai.chat({
-  model: 'chat-bison',
-  temperature: 0.5,
-  messages: [{ author: 'user', content: 'how are you?' }],
-});
-
-console.log(result);
